668ce133fabc37ff1945bafd1d981b47ce63b092
Isabel Palomar <isabel.palomarb@gmail.com>
2018-09-04 23:22:48 -0700

    Greet Service client

---
+++ greet/greet_client/client.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/grpc-golang/greet/greetpb"
+
+	"google.golang.org/grpc"
+)
+
+type server struct{}
+
+func main() {
+	fmt.Println("Hello, I'm a client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("could not connect: %v", err)
+	}
+	defer cc.Close()
+
+	c := greetpb.NewGreetServiceClient(cc)
+	fmt.Printf("Created client: %f", c)
+}
