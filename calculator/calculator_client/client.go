a26dd6e9aefdebc31077f557fdff878a6679d68e
Isabel Palomar <isabel.palomarb@gmail.com>
2018-09-26 22:08:21 -0700

    PrimeNumberDecomposition Server

--- calculator/calculator_client/client.go
+++ calculator/calculator_client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"github.com/grpc-golang/calculator/calculatorpb"
@@ -20,7 +21,9 @@ func main() {
 	defer cc.Close()
 
 	c := calculatorpb.NewCalculatorServiceClient(cc)
-	doUnary(c)
+	//doUnary(c)
+
+	doServerStreaming(c)
 
 }
 
@@ -40,4 +43,28 @@ func doUnary(c calculatorpb.CalculatorServiceClient) {
 	}
 
 	log.Printf("Response from Calculator: %v", res)
+}
+
+func doServerStreaming(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do a PrimeDecomposition Server Streaming RPC ...")
+	req := &calculatorpb.PrimeNumberDecompositionRequest{
+		Number: 1988,
+	}
+
+	stream, err := c.PrimeNumberDecomposition(context.Background(), req)
+
+	if err != nil {
+		log.Fatalf("Error while calling PrimeDecomposition RPC: %v", err)
+	}
+
+	for {
+		res, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("Something happened: %v", err)
+		}
+		fmt.Println(res.GetPrimeFactor())
+	}
 }
